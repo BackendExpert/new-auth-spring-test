0c1c6e4fe040d59ce98d29f62759c557b7bcfbc1
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-12-11 04:35:18 +0530

    Create RegisterRequest.java

---
+++ server/src/main/java/com/jehan/server/dtos/RegisterRequest.java
@@ -0,0 +1,9 @@
+package com.jehan.server.dtos;
+
+public class RegisterRequest {
+    private String username;
+    private String email;
+    private String password;
+    private String Role;
+
+}
